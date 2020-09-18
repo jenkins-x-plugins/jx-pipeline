b98a4e185efb00641083e4a4c855c256d9ee917a
James Strachan <james.strachan@gmail.com>
2020-09-18 12:09:32 +0100

    fix: improve the pipeline

--- promote.sh
+++ promote.sh
@@ -4,4 +4,3 @@ echo "promoting the new version ${VERSION} to downstream repositories"
 
 jx step create pr regex --regex '\s+PipelineVersion = "(?P<version>.*)"' --version ${VERSION} --files pkg/plugins/versions.go --repo https://github.com/jenkins-x/jx-cli.git
 
-jx step create pr regex --regex 'version: (.*)' --version ${VERSION} --files docker/gcr.io/jenkinsxio-labs-private/jx-pipeline.yml --repo https://github.com/jenkins-x/jxr-versions.git
