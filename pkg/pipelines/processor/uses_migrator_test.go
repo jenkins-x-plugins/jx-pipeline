0db8ae38bf8327f8ef119e194d0384d589c1b9b2
James Strachan <james.strachan@gmail.com>
2021-01-21 15:44:24 +0000

    chore: fix broken test

--- pkg/pipelines/processor/uses_migrator_test.go
+++ pkg/pipelines/processor/uses_migrator_test.go
@@ -2,18 +2,20 @@ package processor_test
 
 import (
 	"github.com/jenkins-x/jx-pipeline/pkg/pipelines/processor"
+	"github.com/jenkins-x/lighthouse-client/pkg/triggerconfig/inrepo"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestConvertLegacyStepAnnotationURLToUsesImage(t *testing.T) {
+	key := inrepo.AppendStepURL
 	testCases := []struct {
 		text     string
 		expected string
 	}{
 		{
 			text:     "https://raw.githubusercontent.com/jenkins-x/jx3-pipeline-catalog/60bed6408732c1eda91a15713f51a9f97dcb1757/tasks/git-clone/git-clone-pr.yaml",
-			expected: "uses:jenkins-x/jx3-pipeline-catalog/tasks/git-clone/git-clone-pr.yaml@60bed6408732c1eda91a15713f51a9f97dcb1757",
+			expected: "uses:jenkins-x/jx3-pipeline-catalog/tasks/git-clone/git-clone-pr.yaml@versionStream",
 		},
 		{
 			text:     "https://something/cheese.yaml",
@@ -25,8 +27,17 @@ func TestConvertLegacyStepAnnotationURLToUsesImage(t *testing.T) {
 		},
 	}
 	for _, tc := range testCases {
-		actual := processor.ConvertLegacyStepAnnotationURLToUsesImage(nil, "")
+		ann := map[string]string{
+			key: tc.text,
+		}
+		actual := processor.ConvertLegacyStepAnnotationURLToUsesImage(ann, key)
 		assert.Equal(t, tc.expected, actual, "for annotation value %s", tc.text)
 		t.Logf("converted %s => %s\n", tc.text, actual)
+
+		size := 0
+		if actual == "" {
+			size = 1
+		}
+		assert.Len(t, ann, size, "for annotation value %s", tc.text)
 	}
 }
