b1c2a7b58088367decec240fe02088d38ffb2a68
James Strachan <james.strachan@gmail.com>
2021-03-31 08:05:00 +0100

    fix: add fixes for running inside Jenkins Tekton client
    to ensure we can work without a git token

--- go.mod
+++ go.mod
@@ -6,13 +6,13 @@ require (
 	github.com/cpuguy83/go-md2man v1.0.10
 	github.com/fatih/color v1.10.0
 	github.com/ghodss/yaml v1.0.1-0.20190212211648-25d852aebe32
-	github.com/jenkins-x/go-scm v1.6.7
+	github.com/jenkins-x/go-scm v1.6.9
 	github.com/jenkins-x/jx-api/v4 v4.0.25
 	github.com/jenkins-x/jx-gitops v0.2.37
-	github.com/jenkins-x/jx-helpers/v3 v3.0.93
+	github.com/jenkins-x/jx-helpers/v3 v3.0.94
 	github.com/jenkins-x/jx-kube-client/v3 v3.0.2
 	github.com/jenkins-x/jx-logging/v3 v3.0.3
-	github.com/jenkins-x/lighthouse-client v0.0.85
+	github.com/jenkins-x/lighthouse-client v0.0.86
 	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.7.0
 	github.com/spf13/cobra v1.1.1
---
+++ pkg/cmd/effective/effective_jenkins_integration_test.go
@@ -0,0 +1,156 @@
+package effective_test
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/jenkins-x/jx-helpers/v3/pkg/files"
+	"github.com/jenkins-x/jx-helpers/v3/pkg/testhelpers"
+
+	"github.com/jenkins-x/jx-helpers/v3/pkg/yamls"
+	"github.com/jenkins-x/jx-pipeline/pkg/cmd/effective"
+	"github.com/stretchr/testify/assert"
+	"github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
+
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	gitConfigTemplate = `[core]
+	repositoryformatversion = 0
+	filemode = true
+	bare = false
+	logallrefupdates = true
+	ignorecase = true
+	precomposeunicode = true
+[remote "origin"]
+	url = %s
+	fetch = +refs/heads/*:refs/remotes/origin/*
+`
+)
+
+func TestPipelineEffectiveJenkinsClientWithEnvVar(t *testing.T) {
+	tmpDir, err := ioutil.TempDir("", "")
+	require.NoError(t, err, "could not create temp dir")
+	actual := filepath.Join(tmpDir, "pipeline.yaml")
+	expectedFile := filepath.Join("test_data", ".lighthouse", "jenkins-x", "expected-int.yaml")
+
+	homeDir := filepath.Join(tmpDir, "home")
+	xdgDir := filepath.Join(homeDir, "xdg")
+	err = os.MkdirAll(xdgDir, files.DefaultDirWritePermissions)
+	require.NoError(t, err, "failed to make dirs %s", xdgDir)
+
+	e := map[string]string{
+		"HOME":            homeDir,
+		"XDG_CONFIG_HOME": xdgDir,
+		"GIT_URL":         testGitURL,
+	}
+
+	backupVariables := currentEnv(e)
+	setEnv(e)
+	defer setEnv(backupVariables)
+
+	_, o := effective.NewCmdPipelineEffective()
+
+	o.Dir = "test_data"
+	o.BatchMode = true
+	o.AddDefaults = true
+	o.OutFile = actual
+	o.Resolver = CreateFakeResolver(t)
+	err = o.Run()
+	require.NoError(t, err, "Failed to run linter")
+
+	assert.FileExists(t, actual, "should have generated file")
+
+	pr := &v1beta1.PipelineRun{}
+	err = yamls.LoadFile(actual, pr)
+	require.NoError(t, err, "failed to parse PipelineRun from %s", actual)
+
+	t.Logf("generated valid YAML file %s\n", actual)
+
+	if generateTestOutput {
+		err = files.CopyFile(actual, expectedFile)
+		require.NoError(t, err, "failed to copy %s to %s", actual, expectedFile)
+		return
+	}
+	testhelpers.AssertTextFileContentsEqual(t, actual, expectedFile)
+}
+
+func TestPipelineEffectiveJenkinsClientDiscoverGit(t *testing.T) {
+	tmpDir, err := ioutil.TempDir("", "")
+	require.NoError(t, err, "could not create temp dir")
+	actual := filepath.Join(tmpDir, "pipeline.yaml")
+	expectedFile := filepath.Join("test_data", ".lighthouse", "jenkins-x", "expected-int.yaml")
+
+	err = files.CopyDirOverwrite("test_data", tmpDir)
+	require.NoError(t, err, "failed to copy test_data to %s", tmpDir)
+
+	homeDir := filepath.Join(tmpDir, "home")
+	xdgDir := filepath.Join(homeDir, "xdg")
+	err = os.MkdirAll(xdgDir, files.DefaultDirWritePermissions)
+	require.NoError(t, err, "failed to make dirs %s", xdgDir)
+
+	gitDir := filepath.Join(tmpDir, ".git")
+	err = os.MkdirAll(gitDir, files.DefaultDirWritePermissions)
+	require.NoError(t, err, "failed to make dirs %s", gitDir)
+
+	gitConfig := filepath.Join(gitDir, "config")
+	gitConfigText := fmt.Sprintf(gitConfigTemplate, testGitURL)
+	err = ioutil.WriteFile(gitConfig, []byte(gitConfigText), files.DefaultFileWritePermissions)
+	require.NoError(t, err, "failed to save file %s", gitConfig)
+
+	e := map[string]string{
+		"HOME":            homeDir,
+		"XDG_CONFIG_HOME": xdgDir,
+	}
+
+	backupVariables := currentEnv(e)
+	setEnv(e)
+	defer setEnv(backupVariables)
+
+	_, o := effective.NewCmdPipelineEffective()
+
+	o.Dir = tmpDir
+	o.BatchMode = true
+	o.AddDefaults = true
+	o.OutFile = actual
+	o.Resolver = CreateFakeResolver(t)
+	err = o.Run()
+	require.NoError(t, err, "Failed to run linter")
+
+	assert.FileExists(t, actual, "should have generated file")
+
+	pr := &v1beta1.PipelineRun{}
+	err = yamls.LoadFile(actual, pr)
+	require.NoError(t, err, "failed to parse PipelineRun from %s", actual)
+
+	t.Logf("generated valid YAML file %s\n", actual)
+
+	if generateTestOutput {
+		err = files.CopyFile(actual, expectedFile)
+		require.NoError(t, err, "failed to copy %s to %s", actual, expectedFile)
+		return
+	}
+	testhelpers.AssertTextFileContentsEqual(t, actual, expectedFile)
+}
+
+func currentEnv(env map[string]string) map[string]string {
+	e := map[string]string{}
+	for k := range env {
+		e[k] = os.Getenv(k)
+	}
+	return e
+}
+
+func setEnv(env map[string]string) {
+	for k, v := range env {
+		if v == "" {
+			os.Unsetenv(k)
+		} else {
+			os.Setenv(k, v)
+		}
+	}
+}
