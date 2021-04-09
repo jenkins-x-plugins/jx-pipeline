dd9343e860d131a26beb8b8e56920ab787cf4fc7
James Strachan <james.strachan@gmail.com>
2021-04-09 11:16:21 +0100

    fix: use better build numbers
    when prompting for pipelines to stop lets use a better name with the human friendly build numbers
    
    also lets add filters for branch/context/number

---
+++ pkg/pipelines/activity_resolver.go
@@ -0,0 +1,41 @@
+package pipelines
+
+import (
+	v1 "github.com/jenkins-x/jx-api/v4/pkg/apis/jenkins.io/v1"
+	"github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
+)
+
+// ActivityResolver a helper object to map PipelineRun objects to PipelineActivity resources
+type ActivityResolver struct {
+	activities []v1.PipelineActivity
+	index      map[string]*v1.PipelineActivity
+}
+
+// NewActivityResolver creates a new resolver
+func NewActivityResolver(activities []v1.PipelineActivity) *ActivityResolver {
+	index := map[string]*v1.PipelineActivity{}
+	for i := range activities {
+		a := &activities[i]
+		index[a.Name] = a
+	}
+	return &ActivityResolver{
+		activities: activities,
+		index:      index,
+	}
+}
+
+// ToPipelineActivity converts the given PipelineRun to a PipelineActivity
+func (r *ActivityResolver) ToPipelineActivity(pr *v1beta1.PipelineRun) *v1.PipelineActivity {
+	paName := ToPipelineActivityName(pr, r.activities)
+	if paName == "" {
+		return nil
+	}
+	pa := r.index[paName]
+	if pa == nil {
+		pa = &v1.PipelineActivity{}
+		pa.Name = paName
+		r.index[paName] = pa
+	}
+	ToPipelineActivity(pr, pa, false)
+	return pa
+}
