0984601f61ec8ba78ac6d9c33d0ae65eb8d13ac8
ankitm123 <ankitmohapatra123@gmail.com>
2021-10-08 21:44:42 -0400

    fix: increment build numbers
    Signed-off-by: ankitm123 <ankitmohapatra123@gmail.com>

--- pkg/pipelines/pipelines_test.go
+++ pkg/pipelines/pipelines_test.go
@@ -11,7 +11,18 @@ import (
 	"github.com/jenkins-x/jx-helpers/v3/pkg/yamls"
 	"github.com/stretchr/testify/require"
 	"github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/yaml"
+)
+
+const (
+	branchLabel   = "lighthouse.jenkins-x.io/branch"
+	orgLabel      = "lighthouse.jenkins-x.io/refs.org"
+	repoLabel     = "lighthouse.jenkins-x.io/refs.repo"
+	buildNumLabel = "lighthouse.jenkins-x.io/buildNum"
+	TestOrg       = "jenkins-x-plugins"
+	PipelineRepo  = "jx-pipeline"
+	SecretRepo    = "jx-secret"
 )
 
 func TestInitialPipelineActivity(t *testing.T) {
@@ -79,4 +90,120 @@ func TestMergePipelineActivity(t *testing.T) {
 	t.Logf("created PipelineActivity %s\n", paFile)
 
 	testhelpers.AssertTextFilesEqual(t, filepath.Join("test_data", "merge", "expected.yaml"), paFile, "generated git credentials file")
+}
+
+func generatePipelineRunWithLabels(branch, org, repo, buildNum string) *v1beta1.PipelineRun {
+	return &v1beta1.PipelineRun{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "test-1",
+			Labels: map[string]string{
+				branchLabel:   branch,
+				orgLabel:      org,
+				repoLabel:     repo,
+				buildNumLabel: buildNum,
+			},
+		},
+	}
+}
+
+var paList = []v1.PipelineActivity{
+	{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "jenkins-x-plugins-jx-pipeline-master-2",
+			Labels: map[string]string{
+				branchLabel:   "master",
+				orgLabel:      TestOrg,
+				repoLabel:     PipelineRepo,
+				buildNumLabel: "1601383238723",
+			},
+		},
+	},
+	{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "jenkins-x-plugins-jx-pipeline-master-5",
+			Labels: map[string]string{
+				branchLabel:   "master",
+				orgLabel:      TestOrg,
+				repoLabel:     PipelineRepo,
+				buildNumLabel: "1601383238723",
+			},
+		},
+	},
+	{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "jenkins-x-plugins-jx-secret-master-4",
+			Labels: map[string]string{
+				branchLabel:   "master",
+				orgLabel:      TestOrg,
+				repoLabel:     SecretRepo,
+				buildNumLabel: "1601383238723",
+			},
+		},
+	},
+	{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "jenkins-x-plugins-jx-changelog-master-50",
+			Labels: map[string]string{
+				branchLabel:   "master",
+				orgLabel:      TestOrg,
+				repoLabel:     "jx-changelog",
+				buildNumLabel: "1601383238724",
+			},
+		},
+		Spec: v1.PipelineActivitySpec{
+			Build: "50",
+		},
+	},
+}
+
+var BuildNumberTestCases = []struct {
+	description          string
+	pipelineRun          *v1beta1.PipelineRun
+	paList               []v1.PipelineActivity
+	expectedActivityName string
+}{
+	{
+		"Create pa with build number one for first pipeline of a given prefix",
+		generatePipelineRunWithLabels("master", TestOrg, "jx-promote", "1601383238723"),
+		paList,
+		"jenkins-x-plugins-jx-promote-master-1",
+	},
+	{
+		"Do not Create pa if it already exists for a given prefix",
+		generatePipelineRunWithLabels("master", TestOrg, "jx-changelog", "1601383238724"),
+		paList,
+		"jenkins-x-plugins-jx-changelog-master-50",
+	},
+	{
+		"Create pa by incrementing higher build number for a given prefix - case 1",
+		generatePipelineRunWithLabels("master", TestOrg, PipelineRepo, "1601383238723"),
+		paList,
+		"jenkins-x-plugins-jx-pipeline-master-6",
+	},
+	{
+		"Create pa by incrementing higher build number for a given prefix - case 2",
+		generatePipelineRunWithLabels("master", TestOrg, SecretRepo, "1601383238723"),
+		paList,
+		"jenkins-x-plugins-jx-secret-master-5",
+	},
+	{
+		"Create pa for non master build",
+		generatePipelineRunWithLabels("PR-120", TestOrg, SecretRepo, "1601383238723"),
+		paList,
+		"jenkins-x-plugins-jx-secret-pr-120-1",
+	},
+}
+
+func TestPipelineBuildNumber(t *testing.T) {
+	for _, tt := range BuildNumberTestCases {
+		actualActivityName := ToPipelineActivityName(tt.pipelineRun, tt.paList)
+		t.Log(tt.description)
+		require.Equal(t, actualActivityName, tt.expectedActivityName)
+	}
+}
+
+func BenchmarkPipelineBuildNumber(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		ToPipelineActivityName(generatePipelineRunWithLabels("master", TestOrg, "jx-test", "1601383238723"), paList)
+	}
 }
