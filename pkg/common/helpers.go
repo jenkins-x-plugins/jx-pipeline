87617a02944f5c230ecf7cfb55408148bff0ad4b
Mårten Svantesson <Marten.Svantesson@ticket.se>
2025-02-28 12:24:11 +0100

    fix: show correct usage of sub commands

--- pkg/common/helpers.go
+++ pkg/common/helpers.go
@@ -13,10 +13,10 @@ var TopLevelCommand string
 func init() {
 	BinaryName = os.Getenv("BINARY_NAME")
 	if BinaryName == "" {
-		BinaryName = "jx remote"
+		BinaryName = "jx-pipeline"
 	}
 	TopLevelCommand = os.Getenv("TOP_LEVEL_COMMAND")
 	if TopLevelCommand == "" {
-		TopLevelCommand = "jx remote"
+		TopLevelCommand = "jx-pipeline"
 	}
 }
--- pkg/rootcmd/constants.go
+++
@@ -1,9 +0,0 @@
-package rootcmd
-
-var (
-	// TopLevelCommand top level command
-	TopLevelCommand = "jx-pipeline"
-
-	// BinaryName the name of the command binary in help
-	BinaryName = "jx-pipeline"
-)
